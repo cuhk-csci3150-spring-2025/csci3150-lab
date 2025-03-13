edb9f74f13c5c288d5d7c422989a1572737bc900
Sean <seanyeh2000@gmail.com>
2025-03-13 14:22:12 +0800

    lab 7 files updated

--- lab8_SynchronizationI/process2.c
+++ lab8_SynchronizationI/process2.c
@@ -1,21 +1,21 @@
 /*process-2.c*/
+#include <fcntl.h>
+#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
 #include <sys/stat.h>
-#include <semaphore.h>
 #include <unistd.h>
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[]) {
 
-    char * c = "This is CSCI3150--An operating system course.\n";
+    char *c = "This is CSCI3150--An operating system course.\n";
     // specify no buffering for stderr
     setbuf(stderr, NULL);
 
-    while (* c != '\0') {
-        fputc(* c, stderr);
+    while (*c != '\0') {
+        fputc(*c, stderr);
         c++;
-        sleep(rand()%2+1);
+        sleep(rand() % 2 + 1);
     }
     return 0;
 }
